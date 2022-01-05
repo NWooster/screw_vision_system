29c2f7c93692ae55554e6dd59cf6802be9ad3fc0
Woos100 <73696605+Woos100@users.noreply.github.com>
2022-01-05 19:24:47 +0000

    moved

---
+++ Working_for_picture/main_screw_vision.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+
+Nathan Wooster
+23/12/21
+The main python script to run screw image detection system
+
+'''
+
+def main():
+    '''main function called to run the vision algorithm'''
+
+    x=1
+    y=2
+    z=x+y
+    print(z)
+
+if __name__ == "__main__":
+    main()
