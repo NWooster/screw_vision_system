0c4d9304e8fe946b602bd35e30038b7cfb029126
Woos100 <73696605+Woos100@users.noreply.github.com>
2022-02-02 21:58:48 +0000

    made changes to see if could autofocus before

--- Working_for_picture/take_picture_test.py
+++ Working_for_picture/take_picture_test.py
@@ -1,8 +1,9 @@
 import cv2 as cv
 import time
 
-#cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop)
-cap = cv.VideoCapture(1) # video capture source camera (Here external webcam)
+# cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop)
+cap = cv.VideoCapture(1)  # video capture source camera (Here external webcam)
+print('camera connected')
 
 # Check if the webcam is opened correctly
 if not cap.isOpened():
@@ -13,28 +14,17 @@ if not cap.isOpened():
 while (True):
 
     # Capture the video frame
-    # by frame
     ret, frame = cap.read()
-
     # Display the resulting frame
     cv.imshow('frame', frame)
+    pixel_size = frame.shape[:2]
 
-    #
-
-     #press 'q' button to exit video
+    # press 'q' button to exit video
     if cv.waitKey(1) & 0xFF == ord('q'):
-       break
+        break
 
+print(pixel_size)
 # After the loop release the cap object
 cap.release()
 # Destroy all the windows
 cv.destroyAllWindows()
-
-#while(True):
- #   cv2.imshow('img1',frame) #display the captured image
-  #  if cv2.waitKey(1) & 0xFF == ord('y'): #save on pressing 'y'
-   #     cv2.imwrite('pics/c1.png',frame)
-    #    cv2.destroyAllWindows()
-     #   break
-
-#cap.release()
