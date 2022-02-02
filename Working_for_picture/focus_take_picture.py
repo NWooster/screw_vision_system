772617dc0f64af6f2248277a76e4fc68b8c013cf
Woos100 <73696605+Woos100@users.noreply.github.com>
2022-02-02 22:32:43 +0000

    improved focus time messages

--- Working_for_picture/focus_take_picture.py
+++ Working_for_picture/focus_take_picture.py
@@ -13,12 +13,13 @@ import time
 import numpy as np
 
 
-
 def take_picture(camera):
-    # define camera
+    # define camera (width pixels must be 0.75 of height)
+    pixel_w = 1944  # default 1944
+    pixel_h = pixel_w/0.75  # default 2592 (ratio 0.75)
     cap = cv.VideoCapture(camera)  # video capture source camera (Here external webcam)
-    cap.set(cv.CAP_PROP_FRAME_WIDTH, 2448)
-    cap.set(cv.CAP_PROP_FRAME_HEIGHT, 3264)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, pixel_w)  # set desired pixel width (default 1944)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, pixel_h)  # set desired pixel height (default 2592)
     print('camera connected')
 
     # Check if the webcam is opened correctly
@@ -26,35 +27,35 @@ def take_picture(camera):
         raise IOError("Check if webcam connected properly")  # print and error message
     else:
         start_time = time.time()
-        print(start_time)
-        capture_duration = 10
-
-        while (int(time.time() - start_time) < capture_duration):
+        autofocus_duration = 4
+        print('wait ' + str(autofocus_duration) + ' secs for autofocus')
+        while (int(time.time() - start_time) < autofocus_duration):
             ret, frame = cap.read()
-            if ret == True:
-                print('autofocus in progress')
+            if ret == 1:
+                pass
             else:
                 print('error with frame')
 
-        print('final time:',time.time()-start_time)
+        print('autofocus complete')
 
         ret, frame = cap.read()
 
         pixel_size = frame.shape[:2]
-        print('pixel size:',pixel_size)
+        no_of_pixels = (frame.shape[0] * frame.shape[1])/1000000
+        print('pixel size:', pixel_size)
+        print('no. of pixels:', no_of_pixels, 'MP')
 
         cv.imwrite('image_from_new_cam' + '.jpg', frame)
 
-
     # After the loop release the cap object
     cap.release()
-    #out.release()
+    # out.release()
     # Destroy all the windows
     cv.destroyAllWindows()
 
-#    if cv.waitKey(1) & 0xFF == ord('q'):
- #       cap.release()  # release the camera object
-  #      cv.destroyAllWindows()  # close all other windows
+    #    if cv.waitKey(1) & 0xFF == ord('q'):
+    #       cap.release()  # release the camera object
+    #      cv.destroyAllWindows()  # close all other windows
 
     return 0
 
