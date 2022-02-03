bbafc5f7297ec071120daadf66ab654cfc754c59
Woos100 <73696605+Woos100@users.noreply.github.com>
2022-02-03 15:32:53 +0000

    added comments

--- Working_for_picture/focus_take_picture.py
+++ Working_for_picture/focus_take_picture.py
@@ -1,65 +1,71 @@
 #!/usr/bin/env python3
 
-'''
+"""
 
 Nathan Wooster
 Feb 2022
 Script for accessing camera.
 
-'''
-import sys
+"""
+
+
 import cv2 as cv
 import time
-import numpy as np
 
 
 def take_picture(camera):
+
+    """Take picture function including time to autofocus camera"""
+
     # define camera (width pixels must be 0.75 of height)
     pixel_w = 1944  # default 1944
     pixel_h = pixel_w/0.75  # default 2592 (ratio 0.75)
-    cap = cv.VideoCapture(camera)  # video capture source camera (Here external webcam)
+
+    # connect to camera
+    print('connecting to camera...')
+    cap = cv.VideoCapture(camera)  # video capture source camera (1 is external webcam)
     cap.set(cv.CAP_PROP_FRAME_WIDTH, pixel_w)  # set desired pixel width (default 1944)
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, pixel_h)  # set desired pixel height (default 2592)
     print('camera connected')
 
-    # Check if the webcam is opened correctly
+    # check if the webcam is opened correctly
     if not cap.isOpened():
-        raise IOError("Check if webcam connected properly")  # print and error message
+        raise IOError("Check if webcam connected properly")  # print an error message
     else:
-        start_time = time.time()
-        autofocus_duration = 4
-        print('wait ' + str(autofocus_duration) + ' secs for autofocus')
-        while (int(time.time() - start_time) < autofocus_duration):
-            ret, frame = cap.read()
+        start_time = time.time()  # used for waiting for autofocus
+        autofocus_duration = 4  # seconds
+        print('wait ' + str(autofocus_duration) + ' secs for autofocus...')  # user message
+
+        # begin loop whilst autofocussing, only exits after autofocus time complete
+        while int(time.time() - start_time) < autofocus_duration:
+            ret, frame = cap.read()  # ret is bool if frame captured
             if ret == 1:
                 pass
             else:
-                print('error with frame')
+                print('error with frame capture')
 
+        # autofocus complete and loop exits
         print('autofocus complete')
 
+        # take image (already has autofocussed)
         ret, frame = cap.read()
 
-        pixel_size = frame.shape[:2]
-        no_of_pixels = (frame.shape[0] * frame.shape[1])/1000000
+        pixel_size = frame.shape[:2]  # return pixel resolution x,y
+        no_of_pixels = (frame.shape[0] * frame.shape[1])/1000000  # calc resolution
         print('pixel size:', pixel_size)
         print('no. of pixels:', no_of_pixels, 'MP')
 
-        cv.imwrite('image_from_new_cam' + '.jpg', frame)
+        # save image as filename.jpeg
+        cv.imwrite('latest_image_from_camera' + '.jpg', frame)
 
-    # After the loop release the cap object
+    # release the cap object and close any opened windows
     cap.release()
-    # out.release()
-    # Destroy all the windows
     cv.destroyAllWindows()
 
-    #    if cv.waitKey(1) & 0xFF == ord('q'):
-    #       cap.release()  # release the camera object
-    #      cv.destroyAllWindows()  # close all other windows
-
+    # end of function
     return 0
 
 
 if __name__ == "__main__":
-    camera_no = 1
+    camera_no = 1  # external webcam is 1 not 0
     take_picture(camera_no)
