9ecc4b15255d09a15c2e6341c7abc1c7c2273094
Woos100 <73696605+Woos100@users.noreply.github.com>
2022-02-18 16:46:01 +0000

    image turns up and click addded comments

--- Run_vision_system/ground_truth.py
+++ Run_vision_system/ground_truth.py
@@ -3,8 +3,9 @@
 import cv2 as cv
 import numpy as np
 
+from resize_to_fit_screen import resize
 
-# This will display all the available mouse click events  
+# This will display all the available mouse click events
 events = [i for i in dir(cv) if 'EVENT' in i]
 print(events)
 
@@ -14,27 +15,33 @@ refPt = []
 
 # click event function
 def click_event(event, x, y, flags, param):
+
+    # shows pixel location in blue
     if event == cv.EVENT_LBUTTONDOWN:
-        print(x,",",y)
-        refPt.append([x,y])
+        print(x, ",", y)
+        refPt.append([x, y])
         font = cv.FONT_HERSHEY_SIMPLEX
-        strXY = str(x)+", "+str(y)
-        cv.putText(img, strXY, (x,y), font, 0.5, (255,255,0), 2)
+        strXY = str(x) + ", " + str(y)
+        cv.putText(img, strXY, (x, y), font, 0.5, (255, 255, 0), 2)
         cv.imshow("image", img)
 
+    # shows the colour composition of that pixel in yellow
     if event == cv.EVENT_RBUTTONDOWN:
         blue = img[y, x, 0]
         green = img[y, x, 1]
         red = img[y, x, 2]
         font = cv.FONT_HERSHEY_SIMPLEX
-        strBGR = str(blue)+", "+str(green)+","+str(red)
-        cv.putText(img, strBGR, (x,y), font, 0.5, (0,255,255), 2)
+        strBGR = str(blue) + ", " + str(green) + ", " + str(red)
+        cv.putText(img, strBGR, (x, y), font, 0.5, (0, 255, 255), 2)
         cv.imshow("image", img)
 
 
 # Here, you need to change the image name and it's path according to your directory
 filename = 'images_processed/1screw_output.jpg'
 img = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
+
+# resized_image = resize(img, 600)
+
 cv.imshow("image", img)
 
 # calling the mouse click event
