91f9e01fd8e47e91f412adcc4032affb0321ab07
Woos100 <73696605+Woos100@users.noreply.github.com>
2022-02-25 14:23:11 +0000

    made it be able to crop image and still add pixel locations

--- Run_vision_system/ground_truth.py
+++ Run_vision_system/ground_truth.py
@@ -3,25 +3,21 @@
 import cv2 as cv
 import numpy as np
 
-from resize_to_fit_screen import resize
-
-# This will display all the available mouse click events
-events = [i for i in dir(cv) if 'EVENT' in i]
-print(events)
-
 # This variable we use to store the pixel location
-refPt = []
+pixel_loc = []
 
 
 # click event function
 def click_event(event, x, y, flags, param):
+    # re-adjust x for crop
+    x_adjust = x + x_lower
 
     # shows pixel location in blue
     if event == cv.EVENT_LBUTTONDOWN:
-        print(x, ",", y)
-        refPt.append([x, y])
+        print(x_adjust, ",", y)
+        pixel_loc.append([x_adjust, y])
         font = cv.FONT_HERSHEY_SIMPLEX
-        strXY = str(x) + ", " + str(y)
+        strXY = str(x_adjust) + ", " + str(y)
         cv.putText(img, strXY, (x, y), font, 0.5, (255, 255, 0), 2)
         cv.imshow("image", img)
 
@@ -36,16 +32,43 @@ def click_event(event, x, y, flags, param):
         cv.imshow("image", img)
 
 
-# Here, you need to change the image name and it's path according to your directory
+# specify what side of image to show
+#side_of_image = "left"
+side_of_image = "right"
+
+# image path
 filename = 'images_processed/1screw_output.jpg'
 img = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
 
-# resized_image = resize(img, 600)
+# output image size
+pixel_size = img.shape[:2]
+print(pixel_size)
 
-cv.imshow("image", img)
+if side_of_image == "right":
 
-# calling the mouse click event
-cv.setMouseCallback("image", click_event)
+    # these do not change
+    y_upper = pixel_size[0] - 500
+    y_lower = 0
+    x_upper = 2592
+
+    x_lower = 1920  # IMPORTANT crop place
+
+    # crop image
+    img = img[y_lower:y_upper, x_lower:x_upper]
+
+    # show image
+    cv.imshow("image", img)
+    # calling the mouse click event
+    cv.setMouseCallback("image", click_event)
+
+elif side_of_image == "left":
+
+    x_lower = 0  # for function
+
+    # show image
+    cv.imshow("image", img)
+    # calling the mouse click event
+    cv.setMouseCallback("image", click_event)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
