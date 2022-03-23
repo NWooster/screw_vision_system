b7bb86dd1912c7e87a7f634bbdd13a3f6836984c
Woos100 <73696605+Woos100@users.noreply.github.com>
2022-03-23 18:07:08 +0000

    got rid of error variables as using red dot not cheq square for distance pix to mm

--- Run_vision_system/screw_location.py
+++ Run_vision_system/screw_location.py
@@ -16,7 +16,7 @@ import math
 import calibrate_camera
 
 
-def mm_screw_location(pix_to_mm, origin_pix, ratio_error, image_location='images_taken/1latest_image_from_camera.jpg'):
+def mm_screw_location(pix_to_mm, origin_pix, image_location='images_taken/1latest_image_from_camera.jpg'):
     """
     Screw location function for mm coordinates.
     Calculates it from a given origin (normally top left corner of chessboard is specified).
@@ -33,15 +33,12 @@ def mm_screw_location(pix_to_mm, origin_pix, ratio_error, image_location='images
     # convert to mm coordinates
     mm_locations = pix_loc_from_origin * pix_to_mm
 
-    # find max error in mm
-    max_mm_error = np.amax(abs(mm_locations)) * ratio_error
-
     # find direct distance from origin in pixels and mm
     direct_pix = np.zeros((np.shape(pix_locations)[0], 1))
     direct_mm = np.zeros((np.shape(pix_locations)[0], 1))
     for i in range(np.shape(pix_locations)[0]):
         direct_pix[i] = distance(pix_locations[i, 0], pix_locations[i, 1], origin_pix[0, 0], origin_pix[0, 1])
-        direct_mm[i] = direct_pix[i]*pix_to_mm
+        direct_mm[i] = direct_pix[i] * pix_to_mm
     direct_pix = np.round(direct_pix, 0)  # round to int
     direct_mm = np.round(direct_mm, 2)  # round to 2 decimal place
 
@@ -63,9 +60,9 @@ def mm_screw_location(pix_to_mm, origin_pix, ratio_error, image_location='images
         cv.putText(image, str(direct_mm[i]),
                    (int(pix_locations[i, 0]), int(pix_locations[i, 1]) - 25),
                    font, 0.7, (0, 255, 255), 2)
-        #cv.putText(image, str(pix_loc_from_origin_rounded[i]),
-                   #(int(pix_locations[i, 0]), int(pix_locations[i, 1]) - 10),
-                   #font, 0.7, (255, 255, 255), 2)
+        # cv.putText(image, str(pix_loc_from_origin_rounded[i]),
+        # (int(pix_locations[i, 0]), int(pix_locations[i, 1]) - 10),
+        # font, 0.7, (255, 255, 255), 2)
         cv.putText(image, str(mm_locations_rounded[i]), (int(pix_locations[i, 0]), int(pix_locations[i, 1]) + 15),
                    font, 0.7, (0, 0, 255), 2)
 
@@ -84,7 +81,7 @@ def mm_screw_location(pix_to_mm, origin_pix, ratio_error, image_location='images
     # save image
     cv.imwrite('images_processed/1screw_mm_output' + '.jpg', image)
 
-    return mm_locations, max_mm_error
+    return mm_locations
 
 
 # loads image, pre-process it, apply hough circle detection
@@ -225,21 +222,19 @@ def distance(x1, y1, x2, y2):
 
 
 if __name__ == "__main__":
-
-    pix_to_mm, tl_corner_pix, ratio_error = calibrate_camera.calibrate_camera(image_location='images_taken/'
-                                                                                             '1latest_image_from_'
-                                                                                             'camera.jpg')
+    pix_to_mm, tl_corner_pix, = calibrate_camera.calibrate_camera(image_location='images_taken/'
+                                                                                 '1latest_image_from_'
+                                                                                 'camera.jpg', mm_dist=111)
 
     # test with different mm ratios (normally get from calibrate camera)
-    #mm_ratio = 0.0579  # (from notes calc)
-    #mm_ratio = 0.05765  # (from average of 3 screws notes calc)
-    #mm_ratio = 0.07446754918921591  # from large rectangle cheq square
-    #mm_ratio = 0.05741293532  # (from top right screw)
-    mm_ratio = 0.05718463  # (from top right screw)
+    # mm_ratio = 0.0579  # (from notes calc)
+    # mm_ratio = 0.05765  # (from average of 3 screws notes calc)
+    # mm_ratio = 0.07446754918921591  # from large rectangle cheq square
+    # mm_ratio = 0.05741293532  # (from top right screw)
+    # mm_ratio = 0.05718463  # (from top right screw)
 
-    pix_to_mm = mm_ratio
+    # pix_to_mm = mm_ratio
 
     print('ratio is:' + str(pix_to_mm))
 
-    mm_screw_location(pix_to_mm, tl_corner_pix, ratio_error,
-                      image_location='images_taken/1latest_image_from_camera.jpg')
+    mm_screw_location(pix_to_mm, tl_corner_pix, image_location='images_taken/1latest_image_from_camera.jpg')
