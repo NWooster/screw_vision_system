3488047d7c1d8977ce12f8925d6a8b298ba6a5f1
Woos100 <73696605+Woos100@users.noreply.github.com>
2022-03-24 14:05:53 +0000

    changed pix to mm

--- Run_vision_system/screw_location.py
+++ Run_vision_system/screw_location.py
@@ -224,7 +224,7 @@ def distance(x1, y1, x2, y2):
 if __name__ == "__main__":
     pix_to_mm, tl_corner_pix, = calibrate_camera.calibrate_camera(image_location='images_taken/'
                                                                                  '1latest_image_from_'
-                                                                                 'camera.jpg', mm_dist=111)
+                                                                                 'camera.jpg', mm_dist=79.14)
 
     # test with different mm ratios (normally get from calibrate camera)
     # mm_ratio = 0.0579  # (from notes calc)
